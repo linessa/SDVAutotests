fde5ede79cb50af55f9bb68c7d19c3c9a7d47d92
Marina Gapeeva <m.gapeeva@sdventures.com>
2019-05-28 12:05:32 +0300

    Sample test

---
+++ SDVAutotests/SDVAutotests/SampleClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SDVAutotests
+{
+    public class SampleClass
+    {
+        [Test]
+        public void SampleClassTest()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
