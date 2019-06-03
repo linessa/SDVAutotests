42bea4b681469c75e79fff1669ed7808ce238a50
Marina Gapeeva <m.gapeeva@sdventures.com>
2019-06-03 10:48:57 +0300

    Создан OpenNBCTest и класс NBCHeader

--- SDVAutotests/SDVAutotests/Tests/SampleClass.cs
+++ SDVAutotests/SDVAutotests/Tests/SampleClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
 
 namespace SDVAutotests
 {
@@ -9,6 +10,16 @@ namespace SDVAutotests
         public void SampleClassTest()
         {
             Assert.That(1, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void OpenNBCTest()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.nbc.com/");
+            driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+            driver.Quit();
         }
     }
 }
